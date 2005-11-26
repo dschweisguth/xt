ed1360a760d1b2254cab79ea8ae1d941ecf202d5
Dave Schweisguth <dave@schweisguth.org>
2005-11-26 07:43:01 +0000

    Reformatting

--- src/org/schweisguth/xt/common/domain/Tile.java
+++ src/org/schweisguth/xt/common/domain/Tile.java
@@ -12,36 +12,35 @@ public class Tile implements Comparable, Serializable {
     private static final StickySetMap INSTANCES = new HashStickySetMap();
 
     static {
-        char[][] data =
-            {
-                { 'A', 1 },
-                { 'B', 3 },
-                { 'C', 3 },
-                { 'D', 2 },
-                { 'E', 1 },
-                { 'F', 4 },
-                { 'G', 2 },
-                { 'H', 4 },
-                { 'I', 1 },
-                { 'J', 8 },
-                { 'K', 5 },
-                { 'L', 1 },
-                { 'M', 3 },
-                { 'N', 1 },
-                { 'O', 1 },
-                { 'P', 3 },
-                { 'Q', 10 },
-                { 'R', 1 },
-                { 'S', 1 },
-                { 'T', 1 },
-                { 'U', 1 },
-                { 'V', 4 },
-                { 'W', 4 },
-                { 'X', 8 },
-                { 'Y', 4 },
-                { 'Z', 10 },
-                { ' ', 0 }
-            };
+        char[][] data = {
+            { 'A', 1 },
+            { 'B', 3 },
+            { 'C', 3 },
+            { 'D', 2 },
+            { 'E', 1 },
+            { 'F', 4 },
+            { 'G', 2 },
+            { 'H', 4 },
+            { 'I', 1 },
+            { 'J', 8 },
+            { 'K', 5 },
+            { 'L', 1 },
+            { 'M', 3 },
+            { 'N', 1 },
+            { 'O', 1 },
+            { 'P', 3 },
+            { 'Q', 10 },
+            { 'R', 1 },
+            { 'S', 1 },
+            { 'T', 1 },
+            { 'U', 1 },
+            { 'V', 4 },
+            { 'W', 4 },
+            { 'X', 8 },
+            { 'Y', 4 },
+            { 'Z', 10 },
+            { ' ', 0 }
+        };
         for (int i = 0; i < data.length; i++) {
             char[] datum = data[i];
             char letter = datum[0];
